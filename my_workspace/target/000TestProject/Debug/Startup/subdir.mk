48c9916e266e1e295f36abf0f3f17e137a50e398
hsuankai.chang <hsuankai.chang@driveseres.com>
2022-11-17 17:25:16 -0800

    Add exercise file

--- my_workspace/target/000TestProject/Debug/Startup/subdir.mk
+++ my_workspace/target/000TestProject/Debug/Startup/subdir.mk
@@ -1,6 +1,6 @@
 ################################################################################
 # Automatically-generated file. Do not edit!
-# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
@@ -15,6 +15,13 @@ S_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Startup/startup_stm32f407vgtx.o: ../Startup/startup_stm32f407vgtx.s Startup/subdir.mk
-	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -c -x assembler-with-cpp -MMD -MP -MF"Startup/startup_stm32f407vgtx.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
+Startup/%.o: ../Startup/%.s Startup/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
+
+clean: clean-Startup
+
+clean-Startup:
+	-$(RM) ./Startup/startup_stm32f407vgtx.d ./Startup/startup_stm32f407vgtx.o
+
+.PHONY: clean-Startup
 
